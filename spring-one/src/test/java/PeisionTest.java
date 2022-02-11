6c6cec78a0e75dbcf71bbb095c079c2883fcc612
hcq413 <1725163396@qq.com>
2022-02-11 11:16:01 +0800

    2022-2-11 spring初体验 1.0

--- spring-one/src/test/java/PeisionTest.java
+++ spring-one/src/test/java/PeisionTest.java
@@ -8,8 +8,23 @@ public class PeisionTest {
 
     @Test
     public void show() {
+        long millis = System.currentTimeMillis();
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         Persion persion = (Persion) context.getBean("persion");
         log.info(persion);
+        long millis1 = System.currentTimeMillis();
+        System.out.println("反射创建时间:" + (millis1 - millis)+"ms");
+    }
+
+    @Test
+    public void show1() {
+        long millis = System.currentTimeMillis();
+        Persion persion = new Persion();
+        persion.setId(1);
+        persion.setUsername("hcq413");
+        persion.setPassword("12345678");
+        log.info(persion);
+        long millis1 = System.currentTimeMillis();
+        System.out.println("反射创建时间:" + (millis1 - millis)+"ms");
     }
 }
