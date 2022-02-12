497ca692aa13a3970b04623a79d409c4bff2ba3e
hcq413 <1725163396@qq.com>
2022-02-12 13:49:44 +0800

    2022-2-11 Aop初体验 1.1

--- spring-one/src/test/java/v3Test.java
+++ spring-one/src/test/java/v3Test.java
@@ -1,7 +1,9 @@
 import com.hcq.xxx.one.v3.PrinterUse;
+import lombok.extern.log4j.Log4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+@Log4j
 public class v3Test {
 
     @Test
